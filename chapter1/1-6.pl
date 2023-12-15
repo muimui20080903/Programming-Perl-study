6a66b3be562c01693a5dddc00108fdd6008cc421
muimui20080903 <hiro82585@gmail.com>
2023-12-15 21:45:56 +0900

    complete chapter1!

---
+++ chapter1/1-6.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+# 1.6 制御構造
+$city = "San Francisco";
+if($city  eq "New York"){
+    print "New York!\n";
+}
+elsif($city eq "San Francisco"){
+    print "San Francisco!\n";
+}
+else{
+    print "Some other city!\n";
+}
---
+++ chapter1/1-7.pl
@@ -0,0 +1,5 @@
+#!usr/bin/perl
+# 1.7 正規表現
+# $str = "larry:JYHtPH0./NJTU:100:10:Larrry Wall :/home/larry:/bin/bash";
+# @result = split(/.+:/,$str);
+# print @result[0]."\n";
